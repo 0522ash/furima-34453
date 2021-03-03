7d86bb671065c90f9be0985d06b904fd3f6906f7
0522ash <asukaito0522@gmail.com>
2021-03-03 23:53:38 +0900

    エラーメッセージ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :surname, presence: true
-  validates :firstname, presence: true
-  validates :kana_surname, presence: true
-  validates :kana_firstname, presence: true
+  validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: "Include both letters and numbers"} 
+  validates :surname, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]/, message: "Full-width characters"}
+  validates :firstname, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]/, message: "Full-width characters"}
+  validates :kana_surname, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Kana surname Full-width katakana characters"}
+  validates :kana_firstname, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Kana surname Full-width katakana characters"}
   validates :birthday, presence: true
 end
