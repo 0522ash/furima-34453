00fa5ff148f7802ffd61691e31949ec1536c43a8
0522ash <asukaito0522@gmail.com>
2021-03-04 14:04:20 +0900

    Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: "Include both letters and numbers"} 
-  validates :surname, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]/, message: "Full-width characters"}
-  validates :firstname, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]/, message: "Full-width characters"}
-  validates :kana_surname, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
-  validates :kana_firstname, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+  validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: 'Include both letters and numbers' }
+  validates :surname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/, message: 'Full-width characters' }
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/, message: 'Full-width characters' }
+  validates :kana_surname, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+  validates :kana_firstname, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
   validates :birthday, presence: true
 end
