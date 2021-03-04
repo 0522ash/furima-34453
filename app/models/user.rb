3f3fd649de41f350767ac2a42e00efd0aecaa03a
0522ash <asukaito0522@gmail.com>
2021-03-04 19:24:54 +0900

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,15 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :surname, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'Full-width characters' }
-    validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'Full-width characters' }
-    validates :kana_surname, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
-    validates :kana_firstname,format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
     validates :birthday
     validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'Include both letters and numbers' }
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'Full-width characters' } do
+    validates :surname
+    validates :firstname
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
+    validates :kana_surname
+    validates :kana_firstname
   end
 end
