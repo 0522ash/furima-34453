5166875f96eece34dabc18745c6629283ebb532d
0522ash <asukaito0522@gmail.com>
2021-03-07 20:17:15 +0900

    訂正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :item do
     title { Faker::Lorem.sentence }
-    image { Faker::Lorem.sentence }
     detail { Faker::Lorem.sentence }
     association :user
     category_id           { 2 }
