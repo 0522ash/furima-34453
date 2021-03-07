f933ab196ce1341a508d091ac045bce8f5f41f2a
0522ash <asukaito0522@gmail.com>
2021-03-07 19:38:50 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe Item, type: :model do
       it '販売価格についての情報が必須であること' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank", 'Price Half-width number')
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
       it '売価格は、¥300~¥9,999,999の間のみ保存可能であること' do
@@ -77,10 +77,46 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
 
+      it '値段が10000000円以上の場合は登録できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
+
       it '販売価格は半角数字のみ保存可能であること' do
         @item.price = '１１１１１'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price Out of setting range', 'Price Half-width number')
+        expect(@item.errors.full_messages).to include('Price Half-width number')
+      end
+
+      it 'カテゴリーが1の場合は登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category Select')
+      end
+
+      it '商品の状態が1の場合は登録できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Condition Select')
+      end
+
+      it '配送料の負担が1の場合は登録できない' do
+        @item.shipping_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping charge Select')
+      end
+
+      it '発送までの日数が1の場合は登録できない' do
+        @item.ship_from_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Ship from Select')
+      end
+
+      it '発送元の地域が1の場合は登録できない' do
+        @item.guideline_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Guideline Select')
       end
     end
   end
